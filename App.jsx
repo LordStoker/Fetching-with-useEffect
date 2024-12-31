d3797eef284cbf43d82cf284245e16b99eceb52d
Lord Stoker <tpc00@iesemilidarder.com>
2024-12-31 17:49:02 +0100

    first commit

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
+    
+    /**
+     * Challenge part 1:
+     * Fetch the data from this url: "https://swapi.dev/api/people/1"
+     * and save it in the starWarsData state. Make sure you don't
+     * get stuck in an infinite rendering loop!
+     */
+    
+    React.useEffect( () => {
+        fetch("https://swapi.dev/api/people/1")
+        .then((res) => res.json()
+        .then((data) => setStarWardsData(data)))
+        console.log("Effec redered")
+    }, [count])
+    
+    return (
+        <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
+}
